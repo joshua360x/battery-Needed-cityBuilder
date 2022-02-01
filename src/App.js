bd485b8b744bfedc278c6cb31345bcfbbd8f606a
joshua360x <jkw5499@psu.edu>
2022-02-01 08:59:32 -0500

    set city state for skyline + castle + waterfront + slogan

--- src/App.js
+++ src/App.js
@@ -11,19 +11,24 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
+  const [cityName, setCityName] = useState('Cool Island');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [citySlogan, setCitySlogan] = useState(['Cool', 'pizza', 'time']);
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        {cityName}
       </h1>
-      <div className='bottom'>
+      <div className="bottom">
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
-        <section className='dropdowns'>
+        <section className="dropdowns">
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
           
@@ -32,7 +37,6 @@ function App() {
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-
       </div>
     </div>
   );
